61e7b3ec8e5d98663666d4cefd6d1a530feaf6fc
dFlow3000 <44016633+dFlow3000@users.noreply.github.com>
2018-10-10 11:37:18 +0200

    add new Icon

--- PW/PW/obj/Debug/PW_Content.g.i.cs
+++ PW/PW/obj/Debug/PW_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("unbenannt.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("watten_mai_icon.ico")]
 
 
